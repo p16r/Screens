35859f3db898160abb6810d7e53f659850678ba3
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-10-07 00:05:27 +0530

    Add an ObservableObject shared between device and external screens

---
+++ Screens/ExternalDisplayContent.swift
@@ -0,0 +1,15 @@
+//
+//  ExternalDisplayContent.swift
+//  Screens
+//
+//  Created by Prathamesh Kowarkar on 07/10/20.
+//
+
+import Foundation
+
+class ExternalDisplayContent: ObservableObject {
+
+    @Published var string = ""
+    @Published var isShowingOnExternalDisplay = false
+
+}
