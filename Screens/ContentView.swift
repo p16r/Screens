c4e9bf5b667007d21fc74ed403650e2f8c2ccbe6
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-10-07 00:03:45 +0530

    Start a new project

---
+++ Screens/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Screens
+//
+//  Created by Prathamesh Kowarkar on 06/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+
+}
+
+struct ContentView_Previews: PreviewProvider {
+
+    static var previews: some View {
+        ContentView()
+    }
+
+}
---
+++ Screens/ScreensApp.swift
@@ -0,0 +1,19 @@
+//
+//  ScreensApp.swift
+//  Screens
+//
+//  Created by Prathamesh Kowarkar on 06/10/20.
+//
+
+import SwiftUI
+
+@main
+struct ScreensApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+
+}
