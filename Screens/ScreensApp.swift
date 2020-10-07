384275efba0a441e5dd96f3e19589f4183dc2468
Prathamesh Kowarkar <hello@prathamesh.xyz>
2020-10-07 11:50:19 +0530

    Listen for UIScreen (dis)connect notifications

--- Screens/ScreensApp.swift
+++ Screens/ScreensApp.swift
@@ -5,6 +5,7 @@
 //  Created by Prathamesh Kowarkar on 06/10/20.
 //
 
+import Combine
 import SwiftUI
 
 @main
@@ -12,11 +13,45 @@ struct ScreensApp: App {
 
     @ObservedObject var externalDisplayContent = ExternalDisplayContent()
 
+    private var screenDidConnectPublisher: AnyPublisher<UIScreen, Never> {
+        NotificationCenter.default
+            .publisher(for: UIScreen.didConnectNotification)
+            .compactMap { $0.object as? UIScreen }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+
+    private var screenDidDisconnectPublisher: AnyPublisher<UIScreen, Never> {
+        NotificationCenter.default
+            .publisher(for: UIScreen.didDisconnectNotification)
+            .compactMap { $0.object as? UIScreen }
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(externalDisplayContent)
+                .onReceive(
+                    screenDidConnectPublisher,
+                    perform: screenDidConnect
+                )
+                .onReceive(
+                    screenDidDisconnectPublisher,
+                    perform: screenDidDisconnect
+                )
         }
+    }
+
+    private func screenDidConnect(_ screen: UIScreen) {
+        //  Coming soon…
+        externalDisplayContent.isShowingOnExternalDisplay = true
+    }
+
+    private func screenDidDisconnect(_ screen: UIScreen) {
+        //  Coming soon…
+        externalDisplayContent.isShowingOnExternalDisplay = false
     }
 
 }
